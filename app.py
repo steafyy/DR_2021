8a2247794c474fe2b1dda59673720b4b89e58026
steafyy <78130700+steafyy@users.noreply.github.com>
2021-02-12 22:58:53 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request, url_for, redirect, flash
+from flask import Flask, render_template, request, url_for, redirect, flash, session
 from flask_mysqldb import MySQL
 #from data import Groups
-from wtforms import Form, StringField, TextAreaField, validators
+from passlib.hash import sha256_crypt
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from functools import wraps
 
 
 app = Flask(__name__)
@@ -20,17 +22,110 @@ app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 mysql = MySQL(app)
 
 
-def get_db_connection():
-    cur = mysql.connection.cursor()
-    return cur
-
-
 @app.route('/')
 def main():
     return render_template("index.html")
 
 
+def get_db_connection():
+    cur = mysql.connection.cursor()
+    return cur
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Get Form Fields
+        username = request.form['username']
+        password_candidate = request.form['password']
+
+        # Create cursor
+        cur = mysql.connection.cursor()
+
+        # Get user by username
+        result = cur.execute("SELECT * FROM users WHERE username = %s", [username])
+
+        if result > 0:
+            # Get stored hash
+            data = cur.fetchone()
+            password = data['password']
+
+            # Compare Passwords
+            if sha256_crypt.verify(password_candidate, password):
+                # Passed
+                session['logged_in'] = True
+                session['username'] = username
+
+                flash('You are now logged in', 'success')
+                return redirect(url_for('main'))
+            else:
+                error = 'Invalid login'
+                return render_template('login.html', error=error)
+            # Close connection
+            cur.close()
+        else:
+            error = 'Username not found'
+            return render_template('login.html', error=error)
+
+    return render_template('login.html')
+
+
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Unauthorized, Please login', 'danger')
+            return redirect(url_for('login'))
+    return wrap
+
+
+@app.route('/logout')
+@is_logged_in
+def logout():
+    session.clear()
+    flash('You are now logged out', 'success')
+    return redirect(url_for('login'))
+
+
+class RegisterForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    email = StringField('Email', [validators.Length(min=6, max=50)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords do not match')
+    ])
+    confirm = PasswordField('Confirm Password')
+
+
+@app.route('/register',  methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+
+        cur = mysql.connection.cursor()
+
+        cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)",
+                    (name, email, username, password))
+
+        mysql.connection.commit()
+
+        cur.close()
+
+        flash('You are now registered and can log in', 'success')
+
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+
 @app.route('/devices')
+@is_logged_in
 def devices():
     conn = get_db_connection()
 
@@ -108,12 +203,14 @@ def risks():
 
 
 @app.route('/groups', methods=['POST', 'GET'])
+@is_logged_in
 def groups():
     #print(groups)
     return render_template('groups.html', groups=dev_groups)
 
 
 @app.route('/create_group', methods=['GET', 'POST'])
+@is_logged_in
 def create_group():
     conn = get_db_connection()
 
@@ -136,8 +233,10 @@ def create_group():
 
 
 @app.route('/group')
+@is_logged_in
 def group():
     return render_template('group.html')
+
 
 if __name__ == '__main__':
     app.secret_key = 'super secret key'
